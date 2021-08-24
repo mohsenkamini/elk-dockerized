d58e7811c50563e1885b4a5d818cd1e98a966d6c
mohsenkamini <mohsenkamini@gmail.com>
2021-08-24 14:03:47 +0000

    changed the design of elastic port on the host. 9200 is exposed on the host and maps to nginx container, so that elastic is secured in https

--- docker-compose/ssl/nginx-certbot/init-letsencrypt.sh
+++ docker-compose/ssl/nginx-certbot/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(elk.mohsenkamini.ir)
+domains=(elk.mohsenkamini.ir:9200)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="mohsenkamini@gmail.com" # Adding a valid address is strongly recommended
